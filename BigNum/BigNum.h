5e078eb0c4db03abff261663977c5c6c5e81bcc9
Dave DeLong <davedelong@me.com>
2018-03-14 16:56:46 -0400

    Initial Commit

---
+++ BigNum/BigNum.h
@@ -0,0 +1,19 @@
+//
+//  BigNum.h
+//  BigNum
+//
+//  Created by Dave DeLong on 3/14/18.
+//  Copyright © 2018 Syzygy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for BigNum.
+FOUNDATION_EXPORT double BigNumVersionNumber;
+
+//! Project version string for BigNum.
+FOUNDATION_EXPORT const unsigned char BigNumVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BigNum/PublicHeader.h>
+
+
